90ea035a706a98e1b16ad2d7b3ab7b5983f3d707
Melissa Shields <mshieldsny@gmail.com>
2015-05-14 01:33:48 -0400

    Update styles

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -71,7 +71,7 @@ class EventsController < ApplicationController
     @date_range = []
 
     i = 0
-      while i <= 45
+      while i <= 30
         date_label_pair = []
         if i === 0
           date_label_pair << "Today"
